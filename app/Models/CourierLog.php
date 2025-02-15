f0103c95a3d104f447f400981299bbc4989b2be3
elwyen <demianimrehun@gmail.com>
2025-02-15 08:36:24 +0100

    db base cmf

---
+++ app/Models/CourierLog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourierLog extends Model
+{
+    /** @use HasFactory<\Database\Factories\CourierLogFactory> */
+    use HasFactory;
+}
---
+++ app/Models/RestaurantLog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RestaurantLog extends Model
+{
+    /** @use HasFactory<\Database\Factories\RestaurantLogFactory> */
+    use HasFactory;
+}
