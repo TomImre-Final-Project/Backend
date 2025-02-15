094d42bd59a3a62ef4f5986b579fce5854ff3e47
zaklatomi <hawerock18@gmail.com>
2025-02-15 16:24:27 +0100

    sanctum added, RegisteredUserController

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): Response
+    {
+        $request->validate([
+            'username' => ['required', 'string', 'max:100', 'unique:users'],
+            'email' => ['required', 'email', 'max:100', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+            'role' => ['required', 'in:admin,restaurant_manager,customer,courier'],
+            'phone' => ['required', 'string', 'max:20', 'unique:users'],
+            'address' => ['nullable', 'string', 'max:255'],
+
+        ]);
+
+
+        $user = User::create([
+            'name' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+            'role' => $request->role,
+            'phone' => $request->phone,
+            'address' => $request->address,
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return response()->noContent();
+    }
+}
