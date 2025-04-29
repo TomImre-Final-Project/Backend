e8dfa8adaa7f269ae057dd41e22859db7cb97a1f
zaklatomi <hawerock18@gmail.com>
2025-04-29 08:47:45 +0200

    removed some comments

--- database/factories/DishFactory.php
+++ database/factories/DishFactory.php
@@ -6,9 +6,7 @@ use App\Models\Restaurant;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Dish>
- */
+
 class DishFactory extends Factory
 {
     /**
